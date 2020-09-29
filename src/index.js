17f958c2b56c3a86a47ae28d3094ddd006c58804
Ghiranu <68922813+Ghiranu@users.noreply.github.com>
2020-09-29 19:41:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,7 @@ document.getElementById("form").addEventListener("click", function displayBorder
   
   });
    
-  //  function displayBorder() {
-  //   var elements = document.getElementsByClassName("border");
-  
-  //   for(i=0; i<elements.length; i++) {
-  //     if (elements[i].value === "" ) {
-  //         elements[i].style.borderColor = "red"; 
-  //     } else { elements[i].style.borderColor = "black"; }
-  //     };
-  
-  // }
+
   
   
    
