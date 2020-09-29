54c71c076adf34c4fb24fabb3f33912cb0e2267b
Ghiranu <ionutgrn@yahoo.com>
2020-09-29 19:39:04 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+document.getElementById("form").addEventListener("click", function displayBorder() {
+    var elements = document.getElementsByClassName("border");
+  
+    for(i=0; i<elements.length; i++) {
+      if (elements[i].value === "" ) {
+          elements[i].style.borderColor = "red"; 
+      } else { elements[i].style.borderColor = "black"; }
+      };
+  
+  });
+   
+  //  function displayBorder() {
+  //   var elements = document.getElementsByClassName("border");
+  
+  //   for(i=0; i<elements.length; i++) {
+  //     if (elements[i].value === "" ) {
+  //         elements[i].style.borderColor = "red"; 
+  //     } else { elements[i].style.borderColor = "black"; }
+  //     };
+  
+  // }
+  
+  
+   
+  
