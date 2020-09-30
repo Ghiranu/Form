7833e3e343d93ed539e0be0d85ed4353cfb68d87
Ghiranu <ionutgrn@yahoo.com>
2020-09-30 15:48:34 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
-document.getElementById("form").addEventListener("click", function displayBorder() {
+document.querySelector(".submit").addEventListener("click", function displayBorder(event) {
     var elements = document.getElementsByClassName("border");
   
     for(i=0; i<elements.length; i++) {
       if (elements[i].value === "" ) {
-          elements[i].style.borderColor = "red"; 
-      } else { elements[i].style.borderColor = "black"; }
+          elements[i].classList.add("invalid");
+          event.preventDefault();
+      } else { 
+          elements[i].classList.remove("invalid");
+          
+        }
       };
-  
+      
   });
+
+
+ var showMessage = function () {
+        setTimeout(function(){ 
+          document.querySelector(".valid-message").style.visibility = "visible";
+          var message = document.getElementById("text");
+          var firstInput = document.getElementById("first-input");
+
+          message.innerText += firstInput.value;
+
+          
+          
+
+        }, 1000);
+ };
    
-  //  function displayBorder() {
-  //   var elements = document.getElementsByClassName("border");
+
+
+var closeMessage = document.getElementById("close");
+
+closeMessage.addEventListener("click", function () {
+  window.location.reload();
+});
   
-  //   for(i=0; i<elements.length; i++) {
-  //     if (elements[i].value === "" ) {
-  //         elements[i].style.borderColor = "red"; 
-  //     } else { elements[i].style.borderColor = "black"; }
-  //     };
   
-  // }
+
+
   
+  
+   
+ 
   
    
   
